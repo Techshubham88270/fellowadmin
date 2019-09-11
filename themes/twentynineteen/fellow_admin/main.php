b555ba8a13289fd6a78a9ef4385b3651c61479d4
Techshubham88270 <shubhamsoni0501@gmail.com>
2019-09-11 11:31:26 +0530

    updat with plugin

--- themes/twentynineteen/fellow_admin/main.php
+++ themes/twentynineteen/fellow_admin/main.php
@@ -58,52 +58,110 @@ add_filter( 'manage_users_custom_column', 'add_column_value_agency', 10, 3 );
 * field will add only if agency admin will add users
 */
 
-	//field will add only if agency admin will add users
-	if($check_userrole == "agencyadmin"){
+//field will add only if agency admin will add users
+if($check_userrole == "agencyadmin"){
 
-		/*user_new_form hook for add hidden input field for set agency id
-		*/
-		add_action('user_new_form','agencyid_add_field');
+	/*user_new_form hook for add hidden input field for set agency id
+	*/
+	add_action('user_new_form','agencyid_add_field');
 
-		/*user_register hook for add agency id in meta field of current register user
-		*/
-		add_action( 'user_register', 'agencyid_save_inregistered_usermeta', 100 );
+	/*user_register hook for add agency id in meta field of current register user
+	*/
+	add_action( 'user_register', 'agencyid_save_inregistered_usermeta', 100 );
 
-        /* 
-        * this filter "pre_get_users" for display list of all users for perticular agency
-		*/
-		add_filter('pre_get_users', 'filter_users_by_agencies');
+    /* 
+    * this filter "pre_get_users" for display list of all users for perticular agency
+	*/
+	add_filter('pre_get_users', 'filter_users_by_agencies');
 
-		/* 
-        * remove default roles if agencyadmin user login it can add only agencyadmin/subscriber 
-		*/
+	/* 
+    * remove default roles if agencyadmin user login it can add only agencyadmin/subscriber 
+	*/
+}
+
+
+/*
+* This hook will use to redirect url after login 
+* agencies list page of administrator and user page of agencyadmin 
+*/	
+
+add_filter( 'login_redirect', 'my_login_redirect', 10, 3 );
+
+/*
+* This hook will use to redirect url when any user will access any other page by url 
+* agencies list page of administrator and user page of agencyadmin 
+*/
+add_action('template_redirect', 'custome_url_redirect');
+
+/*
+* This hook will use to Add logo on login page of wordpress
+*  
+*/
+function my_login_logo() 
+{ ?>
+	<style type="text/css">
+	/* css of login page */
+	#login h1 a, .login h1 a {
+	height:65px;
+	width:320px;
+	background-size: 320px 65px;
+	background-repeat: no-repeat;
+    	padding-bottom: 30px;
+
+    }
+	.login-action-login{
+		background-color: #3a90b3;
 	}
+	.login-action-login .fellow_logo{
+		position: absolute;
+		top: 22%;
+		left: 41%;
+		color: #0f0f0f;
+		text-transform: uppercase;
+		font-size: 36px;
 
+	}
+	.login .button-primary {
+		float: none!important;
+		margin-top: 10px!important;
+		width: 100%;
+	}
+	.login-action-login #login {
+		width: 500px;
+		padding: 8% 0 0;
+		margin: auto;
+	}
+	.login-action-login #backtoblog{
+		display:none;
+	}
+	.login-action-login .button.button-large {
+		height: 45px!important;
 
-	/*
-	* This hook will use to redirect url after login 
-	* agencies list page of administrator and user page of agencyadmin 
-	*/	
+	}
+	.login-action-login form{
+		border-radius: 18px!important;
+		position: relative;
+	}
+	.login-action-login .forgetmenot{
+		margin-top: 20px;
 
-	add_filter( 'login_redirect', 'my_login_redirect', 10, 3 );
-    
-    /*
-	* This hook will use to redirect url when any user will access any other page by url 
-	* agencies list page of administrator and user page of agencyadmin 
-	*/
-	add_action('template_redirect', 'custome_url_redirect');
-
-    /*
-	* This hook will use to Add logo on login page of wordpress
-	*  
-	*/
-	function my_login_logo() 
-	{ ?>
-		<style type="text/css">
-				#login h1 a, .login h1 a {
-        background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/site-login-logo.png);
-        }
-		</style>
-	    <h1 class="fellow_logo">Fellow Admin</h1>
-	<?php }
-	add_action( 'login_enqueue_scripts', 'my_login_logo' );
+	}
+	.fellow_logo{
+		color:#fff!important;
+	}
+	.login-action-login #nav{
+		float: right;
+		position: absolute;
+		right: 0;
+		bottom: 100px;
+	}
+	.login-action-login #login{
+		position: relative;
+	}
+	#login h1 a, .login h1 a {
+    background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/site-login-logo.png);
+    }    
+	</style>
+    <h1 class="fellow_logo">Fellow Admin</h1>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
